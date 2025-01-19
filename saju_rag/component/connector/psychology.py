55a754c947116d385c1f4b4a9f1fde4f7750d7f8
chososo <80512711+chososo@users.noreply.github.com>
2025-01-19 21:47:08 +0900

    [ADD] 카네기 도서 검색 추가

--- saju_rag/component/connector/psychology.py
+++ saju_rag/component/connector/psychology.py
@@ -10,15 +10,17 @@ class PsychologyConnector(BaseConnector):
         self.es_repository = es_repository
         self.openai_client = openai_client
         self.index_name = "counseling_data_total"
+        self.model_name = "gpt-4o-mini"
         
     def connector_info(self) -> str:
-        return '{ "connector" : "PsychologyConnector", "description" : "심리학 정보 DB에서 정보를 조회합니다." }'
+        return '{ "connector" : "PsychologyConnector", "description" : "상담 자료와, 인간관계 대응법 DB에서 정보를 조회합니다." }'
 
     def extract_relation(self, query: str) -> str:
         """ query에서 관계 , tag 추출 """
         try :
+            
             response = self.openai_client.chat.completions.create(
-                model="gpt-4o-mini",
+                model= self.model_name,
                 messages=[{
                     "role": "user",
                     "content": f"""사용자 질의를 분석하여 관계 유형과 태그를 추출해주세요.
@@ -43,11 +45,27 @@ class PsychologyConnector(BaseConnector):
         relation_type , tag = self.extract_relation(input.query)
 
         # 유사한 문서 조회
-        similar_documents = self.es_repository.search_documents(input.query, self.index_name, use_semantic_search=True, filters={"relation": relation_type, "tag": tag})
+        # GPT 카운셀링 검색 
+        similar_documents1 = await self.es_repository.search_documents("question",input.query, "counseling_data_total", use_semantic_search=True, filters={"relation": relation_type, "tag": tag})
+        print(similar_documents1)
+        # 카네기 도서 검색 
+        # 고유한 relation 값 집합
+        carnegie_valid_relations = {
+            'career', 'family', 'interpersonal', 'leadership', 
+            'love', 'management', 'other', 'school', 'work'
+        }
+        # relation_type이 유효한 경우에만 필터 추가
+        carnegie_filters = {}
+        if relation_type in carnegie_valid_relations:
+            carnegie_filters["relation"] = relation_type
+        # 카네기 도서 검색 
+        similar_documents2 = await self.es_repository.search_documents("text",input.query, "psychology_book", use_semantic_search=True, filters=carnegie_filters)
+        print(similar_documents2)
         # 조회한 문서를 ConnectorOutput으로 변환
-        results = self.es_repository.parse_response(similar_documents, target_fields=["question", "answer"])
+        results1 = self.es_repository.parse_response(similar_documents1, target_fields=["question", "answer"])
+        results2 = self.es_repository.parse_response(similar_documents2, target_fields=["text", "chapter_name"])
 
-        return results
+        return results1 + results2
 
 
 EXTRACT_TAG_JSON_SCHEMA ={
@@ -56,7 +74,8 @@ EXTRACT_TAG_JSON_SCHEMA ={
                 "relation": {
                     "type": "string",
                     "enum": ["work", "family", "school", "mental_health", "interpersonal", 
-                            "grief", "career", "love", "marriage", "other"]
+                            "grief", "career", "love", "marriage",
+                             "leadership","negotiation","management", "other"]
                 },
                 "tag": {
                     "type": "array",
@@ -64,7 +83,10 @@ EXTRACT_TAG_JSON_SCHEMA ={
                         "type": "string",
                         "enum": ["stress", "anxiety", "depression", "burnout", "conflict",
                                 "adaptation", "communication", "self_esteem", 
-                                "worklife_balance", "relationship", "other"]
+                                "worklife_balance", "relationship", "criticism_handling", "appreciation", "influence", 
+                                "motivation", "leadership_skills", "persuasion",
+                                "conflict_resolution", "active_listening", 
+                                "emotional_intelligence", "positive_attitude",  "other"]
                     }
                 }
             }
